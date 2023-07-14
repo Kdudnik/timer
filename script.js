cafc79edf15a3e9c88258f05798c467b1dd709f5
Kdudnik <kdudnik17@gmail.com>
2023-07-14 11:33:36 +0200

    Add input logic  - inputing of numbers  - reseting of numbers

---
+++ script.js
@@ -0,0 +1,24 @@
+const numbers = document.querySelector('.numbers')
+const hours = document.querySelector('.hours')
+const minutes = document.querySelector('.minutes')
+const seconds = document.querySelector('.seconds')
+const reset = document.querySelector('.reset')
+let inputedTime = `${hours.innerHTML}${minutes.innerHTML}${seconds.innerHTML}`.split('')
+numbers.addEventListener('click', (event) => {
+    if(event.target.classList.contains('numbers')) return
+    inputedTime.push(event.target.innerHTML)
+    inputedTime.shift()
+    translateToTime(inputedTime)
+})
+
+function translateToTime(arr) {
+    hours.innerHTML = `${arr[0]}${arr[1]}`
+    minutes.innerHTML = `${arr[2]}${arr[3]}`
+    seconds.innerHTML = `${arr[4]}${arr[5]}`
+}
+
+reset.addEventListener('click', () => {
+    hours.innerHTML = '00'
+    minutes.innerHTML = '00'
+    seconds.innerHTML = '00'
+})
