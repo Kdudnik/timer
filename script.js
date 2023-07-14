39b414a387e8ba6acf24515b9b1e6c037362f101
Kdudnik <kdudnik17@gmail.com>
2023-07-14 21:30:23 +0200

    Add progress bar, fix bugs

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ const seconds = document.querySelector('.seconds')
 const reset = document.querySelector('[data-submit-type="reset"]')
 const start = document.querySelector('[data-submit-type="start"]')
 const del = document.querySelector('[data-submit-type="delete"]')
+const clock = document.querySelector('.clock')
+const progressBarWrapper = document.querySelector('.progress-bar-wrapper')
+const progressBar = progressBarWrapper.querySelector('.progress-bar')
 let inputedTime = `${hours.innerHTML}${minutes.innerHTML}${seconds.innerHTML}`.split('')
+let pause = false
 numbers.addEventListener('click', (event) => {
     if (event.target.classList.contains('numbers')) return
-    if (!Number(event.target.innerHTML)) return
     inputedTime.push(event.target.innerHTML)
     inputedTime.shift()
     translateToTime(inputedTime)
@@ -19,18 +22,30 @@ reset.addEventListener('click', () => {
     minutes.innerHTML = '00'
     seconds.innerHTML = '00'
     inputedTime = ['0', '0', '0', '0', '0', '0']
+    progressBarWrapper.style.display = "none"
+    numbers.style.display = "flex"
+    pause = true
 })
 
 start.addEventListener('click', () => {
+    if(hours.textContent == '00' && minutes.textContent == '00' && seconds.textContent == '00') return
+    pause = false
+    clock.style.flexGrow = "1"
+    clock.style.marginTop = "0"
+    progressBarWrapper.style.display = "flex"
+    numbers.style.display = "none"
     const goal = new Date().getTime() + convertToTime(inputedTime)
+    progressBar.style.animation = `${Math.floor((goal - new Date().getTime())/1000)}s show-progress linear`
     Countdown.timer(goal, function (time) {
-        document.querySelector('.hours').textContent = ('0' + time.hours).substring(-2)
-        document.querySelector('.minutes').textContent = ('0' + time.minutes).substring(-2)
-        document.querySelector('.seconds').textContent = ('0' + time.seconds).substring(-2)
+        if(pause) return
+        hours.textContent = time.hours.toLocaleString(undefined,{minimumIntegerDigits: 2})
+        minutes.textContent = time.minutes.toLocaleString(undefined,{minimumIntegerDigits: 2})
+        seconds.textContent = time.seconds.toLocaleString(undefined,{minimumIntegerDigits: 2})
     }, function () {
-        document.querySelector('.hours').textContent = '00'
-        document.querySelector('.minutes').textContent = '00'
-        document.querySelector('.seconds').textContent = '00'
+        hours.textContent = '00'
+        minutes.textContent = '00'
+        seconds.textContent = '00'
+        progressBar.style.width = "100%"
     })
 })
 
@@ -40,12 +55,11 @@ function convertToTime(arr) {
     const cHours = Number(`${arr[0]}${arr[1]}`) * 3600000 // 1h = 3600s and 1s = 1000ms
     const cMinutes = Number(`${arr[2]}${arr[3]}`) * 60000 // 1m = 60s and 1s = 1000ms
     const cSeconds = Number(`${arr[4]}${arr[5]}`) * 1000 // 1s = 1000ms
-    console.log(123)
-    console.log(cHours + cMinutes + cSeconds)
     return cHours + cMinutes + cSeconds
 }
 
 function translateToTime(arr) {
+    console.log(arr)
     hours.innerHTML = `${arr[0]}${arr[1]}`
     minutes.innerHTML = `${arr[2]}${arr[3]}`
     seconds.innerHTML = `${arr[4]}${arr[5]}`
